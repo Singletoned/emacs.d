0a4ceeb4eb0747a543cb4fab09205209c8ec8369
Ed Singleton <singletoned@gmail.com>
2011-11-04 15:30:48 +0000

    Moved Yegge-isations to singletoned.el

--- init.el
+++ init.el
@@ -2,11 +2,3 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
-
-(global-set-key (kbd "M-h") 'help-command)
-(global-set-key (kbd "C-h") 'backward-delete-char)
-(global-set-key (kbd "C-w") 'backward-kill-word)
-(global-set-key (kbd "C-x C-k") 'kill-region)
-(global-set-key (kbd "C-c C-k") 'kill-region)
-(global-set-key (kbd "C-x C-m") 'execute-extended-command)
-(global-set-key (kbd "C-c C-m") 'execute-extended-command)
---
+++ singletoned.el
@@ -0,0 +1,8 @@
+;; Some Yegge-isations
+(global-set-key (kbd "M-h") 'help-command)
+(global-set-key (kbd "C-h") 'backward-delete-char)
+(global-set-key (kbd "C-w") 'backward-kill-word)
+(global-set-key (kbd "C-x C-k") 'kill-region)
+(global-set-key (kbd "C-c C-k") 'kill-region)
+(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(global-set-key (kbd "C-c C-m") 'execute-extended-command)
