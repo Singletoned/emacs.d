a21b97c1d86849cb4aefa26dfe47c7c1ececf20d
Ed Singleton <singletoned@gmail.com>
2011-11-07 21:55:35 +0000

    Configured ido mode

--- singletoned.el
+++ singletoned.el
@@ -69,6 +69,20 @@ nil '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\|NOCOMMIT\\)"
         try-expand-all-abbrevs))
 
 
+;; Ido mode
+
+(ido-mode t)
+(ido-ubiquitous t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
+
+
 ;; Keyboard shortcuts
 
 (global-set-key (kbd "M-p") 'backward-paragraph)
