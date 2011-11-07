ac91cd5b0c93490ac37cc117a9224ede8ce0e19d
Ed Singleton <singletoned@gmail.com>
2011-11-07 21:40:27 +0000

    Added the loading of user and system files

--- init.el
+++ init.el
@@ -2,3 +2,9 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+(setq system-config (concat user-emacs-directory system-name ".el")
+      user-config (concat user-emacs-directory user-login-name ".el"))
+
+(when (file-exists-p system-config) (load system-config))
+(when (file-exists-p user-config) (load user-config))
