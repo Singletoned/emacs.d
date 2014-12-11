b6752fe3933b4e59992c5dfd60f1f9c44ee58e09
Ed Singleton <ed.singleton@thisisglobal.com>
2014-12-11 16:35:09 +0000

    Added a modes header comment

--- singletoned.el
+++ singletoned.el
@@ -25,6 +25,9 @@
 (require 'magit)
 (require 'expand-region)
 
+
+;; Modes
+
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
