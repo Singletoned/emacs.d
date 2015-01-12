fadb29d6b5c8bfdd04c3c2fd04b910eb594d350b
Ed Singleton <ed.singleton@thisisglobal.com>
2015-01-12 15:59:12 +0000

    Added a dired create file func

--- singletoned.el
+++ singletoned.el
@@ -112,6 +112,31 @@
           (lambda ()
             (dired-omit-mode 1)))
 
+(eval-after-load 'dired
+  '(progn
+     (define-key dired-mode-map (kbd "_") 'my-dired-create-file)
+     (defun my-dired-create-file (file)
+       "Create a file called FILE. If FILE already exists, signal an error.
+        Stolen from http://stackoverflow.com/questions/2592095/how-do-i-create-an-empty-file-in-emacs"
+       (interactive
+        (list (read-file-name "Create file: " (dired-current-directory))))
+       (let* ((expanded (expand-file-name file))
+              (try expanded)
+              (dir (directory-file-name (file-name-directory expanded)))
+              new)
+         (if (file-exists-p expanded)
+             (error "Cannot create file %s: file exists" expanded))
+         ;; Find the topmost nonexistent parent dir (variable `new')
+         (while (and try (not (file-exists-p try)) (not (equal new try)))
+           (setq new try
+                 try (directory-file-name (file-name-directory try))))
+         (when (not (file-exists-p dir))
+           (make-directory dir t))
+         (write-region "" nil expanded t)
+         (when new
+           (dired-add-file new)
+           (dired-move-to-filename))))))
+
 (require 'ls-lisp)
 (setq ls-lisp-use-insert-directory-program nil)
 (setq ls-lisp-verbosity nil)
