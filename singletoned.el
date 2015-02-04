a22e58539823de2bf8fd5a445c6105ce8411ba85
Ed Singleton <ed.singleton@thisisglobal.com>
2015-02-04 13:48:11 +0000

    Set ido to prompt for new buffer

--- singletoned.el
+++ singletoned.el
@@ -279,7 +279,7 @@ nil '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\|NOCOMMIT\\)"
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-auto-merge-work-directories-length nil
-      ido-create-new-buffer 'always
+      ido-create-new-buffer 'prompt
       ido-use-filename-at-point 'guess
       ido-use-virtual-buffers t
       ido-handle-duplicate-virtual-buffers 2
