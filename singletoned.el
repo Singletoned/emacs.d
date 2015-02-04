dc7fb77c9268d1587cbf2da4749bfabb02c45205
Ed Singleton <ed.singleton@thisisglobal.com>
2015-02-04 13:46:45 +0000

    Rename Python Shell Buffer

--- singletoned.el
+++ singletoned.el
@@ -9,6 +9,8 @@
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
+(set 'python-shell-buffer-name "Python Shell")
+
 ;; Requirements
 
 (require 'saveplace)
