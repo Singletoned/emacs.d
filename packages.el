310c942ae4f25623b63fd7bcd6f1bef2c8e7ed55
Ed Singleton <ed.singleton@thisisglobal.com>
2015-02-05 09:57:44 +0000

    Added web-mode to packages

--- packages.el
+++ packages.el
@@ -4,5 +4,8 @@
   :init (dolist (extension '("yml" "yaml" "sls"))
           (add-to-list 'auto-mode-alist `(,(concat "\\." extension "$") . yaml-mode))))
 
-
+(use-package web-mode
+  :init (dolist (extension '("html" "phtml" "tpl" "php" "jsp" "as[cp]x" "erb" "mustache" "djhtml"))
+          (add-to-list 'auto-mode-alist `(,(concat "\\." extension "\\'") . web-mode)))
+  :config (setq web-mode-engines-alist '(("django" . "\\.html\\'"))))
 
--- singletoned.el
+++ singletoned.el
@@ -33,20 +33,6 @@
 (require 'sws-mode)
 (require 'jade-mode)
 
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
-(setq web-mode-engines-alist
-      '(("django" . "\\.html\\'")))
-
-
 ;; Colors
 
 (set 'custom-safe-themes
