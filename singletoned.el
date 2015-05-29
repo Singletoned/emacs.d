db6953b4be086d24461cfb3ba57123a139af15e3
Ed Singleton <singletoned@gmail.com>
2015-05-29 15:09:07 +0100

    Added reopen last killed file function

--- singletoned.el
+++ singletoned.el
@@ -205,6 +205,15 @@ nil '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\|NOCOMMIT\\)"
 
 ;; Functions
 
+;; Reopen last closed file
+
+(defun find-last-killed-file ()
+  (interactive)
+  (let ((active-files (loop for buf in (buffer-list)
+                            when (buffer-file-name buf) collect it)))
+    (loop for file in recentf-list
+          unless (member file active-files) return (find-file file))))
+
 (defun dired-dwim-copy ()
   "Copy file to other dired window"
   (interactive)
