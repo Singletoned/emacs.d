8470e93fbeb4cce6eafb1a7bf64ec90740e8428a
Ed Singleton <singletoned@gmail.com>
2018-01-31 11:41:59 +0000

    Add dumb-jump

--- packages.el
+++ packages.el
@@ -223,3 +223,12 @@
 
 (use-package google-this
   :ensure t)
+
+(use-package dumb-jump
+  :ensure t
+  :config  (setq dumb-jump-selector 'helm)
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window)))
