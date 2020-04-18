8c122ba43491ed78224468ad75d97de6a7acaa5c
Ed Singleton <singletoned@gmail.com>
2020-04-18 17:01:12 +0100

    Add org date format

--- init.el
+++ init.el
@@ -24,6 +24,8 @@
  '(custom-safe-themes
    (quote
     ("ebe16ce036d354549df68f90586854517b55fbeb738b60708f6f4554e540b801" "2e898e39728a53480a496370cd05fc9923ca1614" "17e8ebc51fc3791f83b2a8853d90a947bca4d814603e6ea8aa42275ff37d915a" "ebf26995fca2e06dfcaaa523e5c9d07f5594f2be" "c0fccdad64d0c5d45b77ae4183076f813fa700f7b474e859aa2dad83955205fa" "b3221d30b128ea2c03ce2f6bf5239b979ca1b6f1" default)))
+ '(org-journal-date-format "%A, %d %B %Y")
+ '(org-journal-dir "~/.journal/")
  '(safe-local-variable-values (quote ((pyvenv-workon . ~/\.env/emacs)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
