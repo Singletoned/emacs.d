0026b938bc098413d06bb50f45ec64ee63d441a9
Ed Singleton <singletoned@gmail.com>
2020-04-18 17:01:39 +0100

    Add node to path

--- singletoned.el
+++ singletoned.el
@@ -6,7 +6,7 @@
 
 ;; Set shell path
 
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(setenv "PATH" (concat "/usr/local/bin:" "~/.emacs.d/node_modules/.bin:" (getenv "PATH")))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
 (set 'python-shell-buffer-name "Python Shell")
