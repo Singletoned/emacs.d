3e52480efd5d4a30ccd5ccb0dbcad41f6400f42e
Ed Singleton <singletoned@gmail.com>
2020-12-09 13:16:22 +0000

    Update init?

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -22,14 +22,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("ebe16ce036d354549df68f90586854517b55fbeb738b60708f6f4554e540b801" "2e898e39728a53480a496370cd05fc9923ca1614" "17e8ebc51fc3791f83b2a8853d90a947bca4d814603e6ea8aa42275ff37d915a" "ebf26995fca2e06dfcaaa523e5c9d07f5594f2be" "c0fccdad64d0c5d45b77ae4183076f813fa700f7b474e859aa2dad83955205fa" "b3221d30b128ea2c03ce2f6bf5239b979ca1b6f1" default)))
+   '("ebe16ce036d354549df68f90586854517b55fbeb738b60708f6f4554e540b801" "2e898e39728a53480a496370cd05fc9923ca1614" "17e8ebc51fc3791f83b2a8853d90a947bca4d814603e6ea8aa42275ff37d915a" "ebf26995fca2e06dfcaaa523e5c9d07f5594f2be" "c0fccdad64d0c5d45b77ae4183076f813fa700f7b474e859aa2dad83955205fa" "b3221d30b128ea2c03ce2f6bf5239b979ca1b6f1" default))
  '(org-journal-date-format "%A, %d %B %Y")
  '(org-journal-dir "~/.journal/")
  '(package-selected-packages
-   (quote
-    (deadgrep elpy dumb-jump google-this spaceline company which-key racket-mode rainbow-delimiters org-journal flx-ido guide-key editorconfig dockerfile-mode expand-region hydra sx json-mode jade-mode magit scratch phi-rectangle string-inflection flycheck phi-search-mc phi-search multiple-cursors helm-projectile helm projectile virtualenvwrapper powerline smartparens centered-cursor-mode yasnippet web-mode yaml-mode use-package)))
- '(safe-local-variable-values (quote ((pyvenv-workon . ~/\.env/emacs)))))
+   '(ess emmet-mode jinja2-mode deadgrep elpy dumb-jump google-this spaceline company which-key racket-mode rainbow-delimiters org-journal flx-ido guide-key editorconfig dockerfile-mode expand-region hydra sx json-mode jade-mode magit scratch phi-rectangle string-inflection flycheck phi-search-mc phi-search multiple-cursors helm-projectile helm projectile virtualenvwrapper powerline smartparens centered-cursor-mode yasnippet web-mode yaml-mode use-package))
+ '(safe-local-variable-values '((pyvenv-workon . ~/\.env/emacs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
