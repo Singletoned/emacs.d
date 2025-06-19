9cfecf1f6c56ead8d0daefdf5d941c550aaa76f6
Ed Singleton <singletoned@gmail.com>
2025-06-19 07:09:13 +0100

    Bootstrap straight

--- init.el
+++ init.el
@@ -1,5 +1,22 @@
 (server-start)
 
+(defvar bootstrap-version)
+(let
+  (
+    (bootstrap-file
+      (expand-file-name "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+          user-emacs-directory)))
+    (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+        'silent
+        'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (setq os-config
   (concat user-emacs-directory (symbol-name system-type) ".el"))
